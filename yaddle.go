d92a28a97b9808f3f8085639eb3e8cc655a2b8bb
julioc98 <julioc.santos98@gmail.com>
2018-02-14 17:41:46 -0200

    add: func GetServers

--- yaddle.go
+++ yaddle.go
@@ -1,1 +1,59 @@
 package yaddle
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"net/http"
+
+	"gitlab-devops.totvs.com.br/golang/yaddle/config"
+)
+
+// Server is a VM into Host
+type Server struct {
+	UUID string `json:"uuid"`
+	Name string `json:"name"`
+}
+
+// Hypervisor is a Compute/Host
+type Hypervisor struct {
+	Status             string   `json:"status"`
+	State              string   `json:"state"`
+	ID                 int      `json:"id"`
+	HypervisorHostname string   `json:"hypervisor_hostname"`
+	Servers            []Server `json:"servers"`
+}
+
+//ServersResponse is a Response of OpenStack Nova API
+type ServersResponse struct {
+	Hypervisors []Hypervisor `json:"hypervisors"`
+}
+
+// GetServers is http request  from OpenStack Nova API
+func GetServers(compute string, authToken string) (*ServersResponse, error) {
+	var response ServersResponse
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	reqUrl := config.OpenStack.BaseUrl + "/v2/8662e6ce659946be9213336d3deaf012/os-hypervisors/compute-2.dev.nuvem-intera.local/servers"
+
+	req, err := http.NewRequest("GET", reqUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("User-Agent", "python-novaclient")
+	req.Header.Add("Accept", "application/json")
+
+	req.Header.Add("X-Auth-Token", authToken)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	json.NewDecoder(resp.Body).Decode(&response)
+
+	return &response, nil
+}
--- yaddle_test.go
+++ yaddle_test.go
@@ -1,9 +1,76 @@
 package yaddle
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"gitlab-devops.totvs.com.br/golang/yaddle/config"
 )
 
-func TestXpto(t *testing.T) {
-	t.Errorf("xyz")
+func MockingServer() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		switch r.URL.Path {
+		case "/v2/8662e6ce659946be9213336d3deaf012/os-hypervisors/compute-2.dev.nuvem-intera.local/servers":
+			// log.Printf("%s", r.Header)
+			if r.Header["X-Auth-Token"][0] == "ABC" {
+				server := Server{
+					UUID: "a67d8b68-47bb-49dd-88ad-8cf9844e62cd",
+					Name: "instance-00003068",
+				}
+
+				hypervisor := Hypervisor{
+					Status:             "enabled",
+					State:              "down",
+					ID:                 12,
+					HypervisorHostname: "compute-2.dev.nuvem-intera.local",
+					Servers:            []Server{server},
+				}
+
+				serversResponse := ServersResponse{
+					Hypervisors: []Hypervisor{hypervisor},
+				}
+				resp, _ := json.Marshal(serversResponse)
+				fmt.Fprintln(w, string(resp))
+			}
+		case "/v2.0/tokens":
+			fmt.Fprintln(w, "ABC")
+		}
+	}))
+}
+
+func Test_GetServers_WithValidHostAndToken_ReturnsValidServersResponse(t *testing.T) {
+
+	server := Server{
+		UUID: "a67d8b68-47bb-49dd-88ad-8cf9844e62cd",
+		Name: "instance-00003068",
+	}
+
+	hypervisor := Hypervisor{
+		Status:             "enabled",
+		State:              "down",
+		ID:                 12,
+		HypervisorHostname: "compute-2.dev.nuvem-intera.local",
+		Servers:            []Server{server},
+	}
+
+	serversResponse := ServersResponse{
+		Hypervisors: []Hypervisor{hypervisor},
+	}
+	res, _ := json.Marshal(serversResponse)
+
+	token := "ABC"
+
+	httpMockingServer := MockingServer()
+	config.OpenStack.BaseUrl = httpMockingServer.URL
+
+	servers, _ := GetServers("compute-2.dev.nuvem-intera.local", token)
+	getServers, _ := json.Marshal(servers)
+
+	if string(res) != string(getServers) {
+		t.Errorf(" Response Mock: %s != Response GetServers: %s", res, getServers)
+	}
 }
