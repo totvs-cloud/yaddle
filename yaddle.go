1f1b9641c00c7886af90c8ff5c5e9eff83512460
julioc98 <julioc.santos98@gmail.com>
2018-02-19 14:53:53 -0300

    add: AuthGetToken

--- yaddle.go
+++ yaddle.go
@@ -4,9 +4,45 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"net/http"
+	"strings"
+	"time"
 
 	"gitlab-devops.totvs.com.br/golang/yaddle/config"
 )
+
+// Token is a auth token for OpenStack APIs where ID is the token value
+type Token struct {
+	IssuedAt string    `json:"issued_at"`
+	Expires  time.Time `json:"expires"`
+	ID       string    `json:"id"`
+}
+
+// Access takes the Token
+type Access struct {
+	Token Token `json:"token"`
+}
+
+// AuthResponse is a response for OpenStack auth API where contains a tokenjuilioa
+type AuthResponse struct {
+	Access Access `json:"access"`
+}
+
+// PasswordCredentials contains infor for access on OpenStack
+type PasswordCredentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Auth is where passwordCredentials and id for tenant("master compute")
+type Auth struct {
+	PasswordCredentials PasswordCredentials `json:"passwordCredentials"`
+	TenantID            string              `json:"tenantId"`
+}
+
+// AuthOpenStack is a payload for request on OpenStack auth API
+type AuthOpenStack struct {
+	Auth Auth `json:"auth"`
+}
 
 // Server is a VM into Host
 type Server struct {
@@ -28,6 +64,52 @@ type ServersResponse struct {
 	Hypervisors []Hypervisor `json:"hypervisors"`
 }
 
+// AuthGetToken is http request from OpenStack auth API
+func AuthGetToken() (*Token, error) {
+
+	var response AuthResponse
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	reqURL := config.OpenStack.AuthUrl + "/v2.0/tokens"
+
+	authPayload := AuthOpenStack{
+		Auth: Auth{
+			PasswordCredentials: PasswordCredentials{
+				Username: config.OpenStack.Username,
+				Password: config.OpenStack.Password,
+			},
+			TenantID: config.OpenStack.TenantID,
+		},
+	}
+
+	authJSON, err := json.Marshal(authPayload)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", reqURL, strings.NewReader(string(authJSON)))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("User-Agent", "python-novaclient")
+	req.Header.Add("Accept", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	json.NewDecoder(resp.Body).Decode(&response)
+
+	return &response.Access.Token, nil
+
+}
+
 // GetServers is http request  from OpenStack Nova API
 func GetServers(compute string, authToken string) (*ServersResponse, error) {
 	var response ServersResponse
@@ -37,9 +119,9 @@ func GetServers(compute string, authToken string) (*ServersResponse, error) {
 	}
 	client := &http.Client{Transport: tr}
 
-	reqUrl := config.OpenStack.BaseUrl + "/v2/8662e6ce659946be9213336d3deaf012/os-hypervisors/compute-2.dev.nuvem-intera.local/servers"
+	reqURL := config.OpenStack.BaseUrl + "/v2/" + config.OpenStack.TenantID + "/os-hypervisors/" + compute + "/servers"
 
-	req, err := http.NewRequest("GET", reqUrl, nil)
+	req, err := http.NewRequest("GET", reqURL, nil)
 	if err != nil {
 		return nil, err
 	}
--- yaddle_test.go
+++ yaddle_test.go
@@ -6,50 +6,80 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"gitlab-devops.totvs.com.br/golang/yaddle/config"
 )
 
-func MockingServer() *httptest.Server {
-	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// AuthGetToken
+var (
 
-		switch r.URL.Path {
-		case "/v2/8662e6ce659946be9213336d3deaf012/os-hypervisors/compute-2.dev.nuvem-intera.local/servers":
-			// log.Printf("%s", r.Header)
-			if r.Header["X-Auth-Token"][0] == "ABC" {
-				server := Server{
-					UUID: "a67d8b68-47bb-49dd-88ad-8cf9844e62cd",
-					Name: "instance-00003068",
-				}
+	// Response
+	token        Token
+	access       Access
+	authResponse AuthResponse
 
-				hypervisor := Hypervisor{
-					Status:             "enabled",
-					State:              "down",
-					ID:                 12,
-					HypervisorHostname: "compute-2.dev.nuvem-intera.local",
-					Servers:            []Server{server},
-				}
+	// Request
+	passwordCredentials PasswordCredentials
+	auth                Auth
+	authOpenStack       AuthOpenStack
+)
 
-				serversResponse := ServersResponse{
-					Hypervisors: []Hypervisor{hypervisor},
-				}
-				resp, _ := json.Marshal(serversResponse)
-				fmt.Fprintln(w, string(resp))
-			}
-		case "/v2.0/tokens":
-			fmt.Fprintln(w, "ABC")
-		}
-	}))
+// GetServers
+var (
+	server          Server
+	hypervisor      Hypervisor
+	serversResponse ServersResponse
+
+	authToken string
+)
+
+//AuthGetToken
+func init() {
+
+	t, _ := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
+
+	//AuthGetToken Response
+	token = Token{
+		IssuedAt: "2018-02-14T19:42:42.848806",
+		Expires:  t,
+		ID:       "XYZ",
+	}
+
+	access = Access{
+		Token: token,
+	}
+
+	authResponse = AuthResponse{
+		Access: access,
+	}
+
+	//AuthGetToken Request
+	passwordCredentials = PasswordCredentials{
+		Username: "jc123",
+		Password: "jc123",
+	}
+
+	auth = Auth{
+		PasswordCredentials: passwordCredentials,
+		TenantID:            "8662e6ce659946be9213346d3deaf013",
+	}
+
+	authOpenStack = AuthOpenStack{
+		Auth: auth,
+	}
+
 }
 
-func Test_GetServers_WithValidHostAndToken_ReturnsValidServersResponse(t *testing.T) {
+// GetServers
+func init() {
 
-	server := Server{
+	server = Server{
 		UUID: "a67d8b68-47bb-49dd-88ad-8cf9844e62cd",
 		Name: "instance-00003068",
 	}
 
-	hypervisor := Hypervisor{
+	hypervisor = Hypervisor{
 		Status:             "enabled",
 		State:              "down",
 		ID:                 12,
@@ -57,17 +87,76 @@ func Test_GetServers_WithValidHostAndToken_ReturnsValidServersResponse(t *testin
 		Servers:            []Server{server},
 	}
 
-	serversResponse := ServersResponse{
+	serversResponse = ServersResponse{
 		Hypervisors: []Hypervisor{hypervisor},
 	}
+
+	authToken = "ABC"
+
+}
+
+// Configs
+func init() {
+
+	config.OpenStack.Username = passwordCredentials.Username
+	config.OpenStack.Password = passwordCredentials.Password
+
+	config.OpenStack.TenantID = auth.TenantID
+
+}
+
+func MockingServer() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		switch r.URL.Path {
+
+		case "/v2.0/tokens":
+			var reqToken AuthOpenStack
+			defer r.Body.Close()
+			json.NewDecoder(r.Body).Decode(&reqToken)
+
+			if reqToken.Auth.PasswordCredentials.Password == config.OpenStack.Password &&
+				reqToken.Auth.PasswordCredentials.Username == config.OpenStack.Username &&
+				reqToken.Auth.TenantID == config.OpenStack.TenantID {
+
+				resp, _ := json.Marshal(authResponse)
+				fmt.Fprintln(w, string(resp))
+			}
+
+		case "/v2/" + config.OpenStack.TenantID + "/os-hypervisors/compute-2.dev.nuvem-intera.local/servers":
+			if r.Header["X-Auth-Token"][0] == authToken {
+				resp, _ := json.Marshal(serversResponse)
+				fmt.Fprintln(w, string(resp))
+			}
+
+		}
+	}))
+}
+
+func Test_AuthGetToken_WithValidConfig_ReturnsValidAuthToken(t *testing.T) {
+	res, _ := json.Marshal(token)
+
+	// TODO: Ver se não é melhor usar a propriedade RequestURI
+	httpMockingServer := MockingServer()
+	config.OpenStack.AuthUrl = httpMockingServer.URL
+
+	aToken, _ := AuthGetToken()
+	authToken, _ := json.Marshal(aToken)
+
+	if string(res) != string(authToken) {
+		t.Errorf(" Response Mock: %s != Response AuthGetToken: %s", res, authToken)
+	}
+}
+
+func Test_GetServers_WithValidHostAndToken_ReturnsValidServersResponse(t *testing.T) {
+
 	res, _ := json.Marshal(serversResponse)
 
-	token := "ABC"
-
+	// TODO: Ver se não é melhor usar a propriedade RequestURI
 	httpMockingServer := MockingServer()
 	config.OpenStack.BaseUrl = httpMockingServer.URL
 
-	servers, _ := GetServers("compute-2.dev.nuvem-intera.local", token)
+	servers, _ := GetServers("compute-2.dev.nuvem-intera.local", authToken)
 	getServers, _ := json.Marshal(servers)
 
 	if string(res) != string(getServers) {
